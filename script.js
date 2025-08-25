2ac3916e365e2371016c5aebaa955c7614d0851f
maiara77 <161398179+maiara77@users.noreply.github.com>
2025-08-25 22:20:31 +0000

    salvar

--- script.js
+++ script.js
@@ -6,24 +6,24 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Quando você acorda se sente mais disposto",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "afirmação"
+                texto: "pela manhã com o sol iluminando tudo",
+                afirmacao: "prefiro aproveitar a energia do dia"
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "afirmação"
+                texto: "a noite, quando tudo está mais calmo",
+                afirmacao: "gosto da tranquilidade e silêncio da noite"
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "na hora de estudar ou trabalhar você se sente mais produtivo",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "afirmação"
+                texto: "durante o dia com a luz natural",
+                afirmacao: "minha mente rende melhor com a luz do sol"
             },
             {
                 texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
